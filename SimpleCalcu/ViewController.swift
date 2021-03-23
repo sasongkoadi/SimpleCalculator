ae87a9eeba16a6ab1771b2fff88f0c2b0e9d6603
Sasongko Adi <sasongko.adinugroho@qmail.id>
2021-03-24 06:25:57 +0800

    add feature calculate

--- SimpleCalcu/ViewController.swift
+++ SimpleCalcu/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var length: UITextField!
+    @IBOutlet weak var width: UITextField!
+    @IBOutlet weak var result: UILabel!
+    @IBOutlet weak var calculate: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
+    @IBAction func calculateResult(_ sender: Any) {
+        if let l = Double(length.text ?? "0" ), let w = Double(width.text ?? "0") {
+            calculate.setTitle("Hapus", for: .normal)
+            result.text = String(l * w)
+        } else {
+            result.text = "Hasil Tidak Valid"
+        }
+        
+    }
+    
+    @IBAction func resetResult(_ sender: Any) {
+        length.text = ""
+        width.text = ""
+        result.text = "Hasil"
+        calculate.setTitle("Hitung", for: .normal)
+    }
 }
 
